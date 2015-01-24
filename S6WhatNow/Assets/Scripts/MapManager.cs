a61b8963fda6ee9f888f7c3b6bad1d377dda38f4
Reinan Contawi <reinancntw@gmail.com>
2015-01-25 04:23:26 +0800

    Map functionalities

---
+++ S6WhatNow/Assets/Scripts/MapManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapManager : MonoBehaviour
+{
+	private static MapManager m_instance;
+	public static MapManager Instance
+	{
+		get
+		{
+			if ( m_instance == null )
+			{
+				m_instance = GameObject.FindObjectOfType<MapManager>();
+			}
+			return m_instance;
+		}
+	}
+
+	[SerializeField] private Map[] m_maps;
+
+	private void Start ()
+	{
+	
+	}
+
+	private void DeactivateMapObject (int p_mapIndex, int p_mapObjectIndex)
+	{
+
+	}
+}
